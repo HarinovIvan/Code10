9f7c76bde14f9ec39e96e8f7b7e016c5123f256d
Harinov_Ivan <harinovivan410@gmail.com>
2025-02-24 21:46:46 +0300

    Сделана практика a

--- Lesson1/ReadySolution/obj/Debug/net9.0/ReadySolution.AssemblyInfo.cs
+++ Lesson1/ReadySolution/obj/Debug/net9.0/ReadySolution.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReadySolution")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3806509c48e58efe1d955dba9cb20fd6170cae1e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+265f37cb936b1762d0be07d7ca651abc5343e2ab")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReadySolution")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReadySolution")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
