401d40d4dddfc8ef295e3185d88b32f2fbede03f
Harinov_Ivan <harinovivan410@gmail.com>
2025-04-14 19:18:10 +0300

    сделана практика

--- Lesson11/CSProject/Program.cs
+++ Lesson11/CSProject/Program.cs
@@ -1,99 +1,68 @@
-﻿class Program
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+public delegate double MathOperation(double x, double y);
+
+class Program
 {
-    static void Main()
+    public static double Add(double x, double y)
     {
-        //A
-        MathOperation addition = Add;
-        MathOperation multiplication = Multiply;
-        Console.WriteLine("Сложение: " + ExecuteOperation(addition, 1, 4));
-        Console.WriteLine("Умножение: " + ExecuteOperation(multiplication, 5, 3));
-
-        //B
-        TemperatureSensor sensor = new TemperatureSensor();
-        sensor.TemperatureChanged += (sender, e) =>
-        {
-            Console.WriteLine("Температура изменилась!");
-        };
-
-        sensor.UpdateTemperature(30.2);
-        sensor.UpdateTemperature(180.0);
-
-
-        //C
-        Stock stock = new Stock();
-
-            stock.PriceChanged += (sender, e) =>
-            {
-                Console.WriteLine("Цена изменилась!");
-            };
-
-            stock.PriceChanged += (sender, e) =>
-            {
-                Console.WriteLine("Новая цена: " + ((Stock)sender).Price);
-            };
-
-            stock.ChangePrice(123.5);
-            stock.ChangePrice(89.0);
+        return x + y;
     }
 
-        //A
-        //1
-        delegate double MathOperation(double x, double y);
+    public static double Multiply(double x, double y)
+    {
+        return x * y;
+    }
 
-        //2
-        static double Add(double x, double y)
-        {
-            return x + y;
-        }
+    public static double ExecuteOperation(MathOperation operation, double x, double y)
+    {
+        return operation(x, y);
+    }
 
-        static double Multiply(double x, double y)
-        {
-            return x * y;
-        }
+    public static void Main()
+    {
+        MathOperation handler = Multiply;
+        double i = ExecuteOperation(handler, 5, 5);
+        Console.WriteLine(i);
+        handler = Add;
+        i = ExecuteOperation(handler, 5, 5);
+        Console.WriteLine(i);
 
-        //3
-        static double ExecuteOperation(MathOperation operation, double x, double y)
+        TemperatureSensor sensor = new TemperatureSensor();
+        
+        sensor.TemperatureChanged += (send, temp) => 
         {
-            return operation(x, y);
-        }
-    
+            System.Console.WriteLine($"Температура: {temp} C");
+        };
+        
+        sensor.UpdateTemperature(50);
+        sensor.UpdateTemperature(30);
+        sensor.UpdateTemperature(84);
+        sensor.UpdateTemperature(14);
+        sensor.UpdateTemperature(43);
+    }
 }
 
-//B
+
+
 class TemperatureSensor
+{
+    private double _Temperature;
+    public double Temperature => _Temperature; 
+    public event EventHandler<double> TemperatureChanged;
+
+    public void UpdateTemperature(double newTemperature)
     {
-        private double temperature;
-
-        //1
-        public double Temperature
+        if (newTemperature != _Temperature)
         {
-            get { return temperature; }
-            set
-            {
-                if (temperature != value)
-                {
-                    temperature = value;
-                    OnTemperatureChanged();
-                }
-            }
-        }
+            _Temperature = newTemperature;
 
-        //2
-        public event EventHandler TemperatureChanged;
-
-        public void UpdateTemperature(double newTemperature)
-        {
-            Temperature = newTemperature;
-        }
-
-        protected virtual void OnTemperatureChanged()
-        {
-            TemperatureChanged?.Invoke(this, EventArgs.Empty);
+            TemperatureChanged?.Invoke(this, _Temperature);
         }
     }
+}
 
-
-//C
 class Stock
     {
         private double price;
@@ -101,15 +70,12 @@ class Stock
         //1
         public double Price
         {
-            get { return price; }
-            set
+            if (price != newPrice)
             {
-                if (price != value)
-                {
-                    price = value;
-                    OnPriceChanged();
-                }
+                newPrice = price;
+                OnPriceChanged();
             }
+            
         }
 
         //2
--- Program.cs
+++ Program.cs
@@ -1,19 +1,93 @@
-﻿namespace Code10;
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+public delegate double MathOperation(double x, double y);
 
 class Program
 {
-    static void Main(string[] args)
+    public static double Add(double x, double y)
     {
-        // WriteLine - вывод в консоль
-        Console.WriteLine("Hello, World!");
+        return x + y;
+    }
 
-        // ReadLine - ввод с клавиатуры
-        Console.ReadLine();
+    public static double Multiply(double x, double y)
+    {
+        return x * y;
+    }
 
-        int age = 20;
-        string name = "John";
-        double pi = 3.14;
-        bool isMale = false;
+    public static double ExecuteOperation(MathOperation operation, double x, double y)
+    {
+        return operation(x, y);
+    }
+
+    public static void Main()
+    {
+        MathOperation handler = Multiply;
+        double i = ExecuteOperation(handler, 5, 5);
+        Console.WriteLine(i);
+        handler = Add;
+        i = ExecuteOperation(handler, 5, 5);
+        Console.WriteLine(i);
+
+        TemperatureSensor sensor = new TemperatureSensor();
         
+        sensor.TemperatureChanged += (send, temp) => 
+        {
+            System.Console.WriteLine($"Температура: {temp} C");
+        };
+        
+        sensor.UpdateTemperature(50);
+        sensor.UpdateTemperature(30);
+        sensor.UpdateTemperature(84);
+        sensor.UpdateTemperature(14);
+        sensor.UpdateTemperature(43);
     }
 }
+
+
+
+class TemperatureSensor
+{
+    private double _Temperature;
+    public double Temperature => _Temperature; 
+    public event EventHandler<double> TemperatureChanged;
+
+    public void UpdateTemperature(double newTemperature)
+    {
+        if (newTemperature != _Temperature)
+        {
+            _Temperature = newTemperature;
+
+            TemperatureChanged?.Invoke(this, _Temperature);
+        }
+    }
+}
+
+class Stock
+    {
+        private double price;
+
+        //1
+        public double Price
+        {
+            if (price != newPrice)
+            {
+                newPrice = price;
+                OnPriceChanged();
+            }
+            
+        }
+
+        //2
+        public event EventHandler PriceChanged;
+
+        public void ChangePrice(double newPrice)
+        {
+            Price = newPrice;
+        }
+
+        protected virtual void OnPriceChanged()
+        {
+            PriceChanged?.Invoke(this, EventArgs.Empty);
+        }
+    }
